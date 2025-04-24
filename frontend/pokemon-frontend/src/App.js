643cfd686b9adf2bde7e5790674d196f7ae74c58
Shivani Yadav <shivaniyadav1912@gmail.com>
2025-04-24 20:04:36 +0530

    changes to all file

--- frontend/pokemon-frontend/src/App.js
+++ frontend/pokemon-frontend/src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import Home from './components/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PokemonDetail from '../src/components/Detailscreen';
+import PokemonDetail from '../src/components/Detailscreen'; 
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/pokemon/:id" element={<PokemonDetail />} />
+      
       </Routes>
     </Router>
   );
