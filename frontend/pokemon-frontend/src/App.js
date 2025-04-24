9f508a50de8a47fe8dd5a827d2d88070cf1e14bf
Shivani Yadav <shivaniyadav1912@gmail.com>
2025-04-24 20:56:42 +0530

    changes to all file

--- frontend/pokemon-frontend/src/App.js
+++ frontend/pokemon-frontend/src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
+      <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/pokemon/:id" element={<PokemonDetail />} />
       
